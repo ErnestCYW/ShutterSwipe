a26e74e70da6c53c8f5156ad60ea67cb6882a8df
ErnestCYW <ernestcyw99@gmail.com>
2021-06-03 21:32:03 +0800

    add prelim picture upload feature

--- client/src/components/Dashboard.js
+++ client/src/components/Dashboard.js
@@ -1,10 +1,14 @@
 import React, { Fragment, useState, useEffect } from "react";
 import { toast } from "react-toastify";
+import axios from 'axios';
 
 const Dashboard = ({ setAuth }) => {
   //passing prop setAuth
 
   const [name, setName] = useState("");
+  const [file, setFile] = useState("");
+  const [filename, setFilename] = useState("Choose File");
+  const [uploadedFile, setUploadedFile] = useState({})
 
   async function getName() {
     try {
@@ -28,6 +32,34 @@ const Dashboard = ({ setAuth }) => {
     toast.success("Logged out successfully");
   };
 
+  const stageFile = e => {
+    setFile(e.target.files[0]);     //one file upload only
+    setFilename(e.target.files[0].name);
+  };
+
+  const uploadFile = async e => {
+    e.preventDefault();
+    const formData = new FormData();
+    formData.append('file',file);
+
+    try {
+      const res = await axios.post('/upload', formData, {
+          headers: {
+            'Content-Type': 'multipart/form-data'
+          }
+      });
+
+      const {fileName, filePath} = res.data;
+      setUploadedFile({fileName, filePath});
+    } catch(err) {
+      if (err.response.status === 500) {
+        console.log('Server Error');
+      } else {
+        console.log(err.response.data.msg);
+      }
+    }
+  };
+
   useEffect(() => {
     getName();
   }, []); //bracket makes one request only
@@ -38,6 +70,15 @@ const Dashboard = ({ setAuth }) => {
       <button className="btn btn-primary" onClick={(e) => logout(e)}>
         Logout
       </button>
+      <form onSubmit = {uploadFile}>
+        <div className="mb-3">
+          <label for="formFile" className="form-label">{filename}</label>
+          <input className="form-control" type="file" 
+            id="formFile" multiple onChange={stageFile}/>
+        </div>
+
+        <input type="submit" value="Upload" className = "btn btn-primary btn-block mt-4" />
+      </form>
     </Fragment>
   );
 };
