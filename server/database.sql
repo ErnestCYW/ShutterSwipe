729e180edcc23ba0c816fc27d9721336d9d409bf
ErnestCYW <ernestcyw99@gmail.com>
2021-06-05 18:57:22 +0800

    edited database.sql to include pics

--- server/database.sql
+++ server/database.sql
@@ -1,6 +1,8 @@
-CREATE DATABASE shutterswipe;
+CREATE DATABASE ShutterSwipe;
 
---Base off database schema. 
+--BASED OF DATABASE SCHEMA
+
+--User Table
 CREATE TABLE users (
   user_id UUID PRIMARY KEY DEFAULT uuid_generate_v4(), --create UUID OSSP if not installed
   user_name VARCHAR(100) NOT NULL,
@@ -8,6 +10,23 @@ CREATE TABLE users (
   user_password VARCHAR(100) NOT NULL
 )
 
---insert fake user
-INSERT INTO users (user_name, user_email, user_password) VALUES
-('henry', 'henry123@gmail.com', 'password1');
+--Picture table
+CREATE TABLE pics (
+  pic_id UUID PRIMARY KEY DEFAULT uuid_generate_v4(),
+  user_id UUID REFERENCES users(user_id) NOT NULL
+)
+
+--TESTING
+
+--Insert Fake User & Pics For Debugging Purposes
+INSERT INTO users (user_id, user_name, user_email, user_password) VALUES
+(DEFAULT, 'test_name', 'test@gmail.com', 'test_password');
+
+--Create 2 New Pictures
+INSERT INTO pics (pic_id,user_id) VALUES
+(DEFAULT, 'dcf7cee5-2f02-4cec-9090-618309c3714a' ); --your uuid (user_id) will be different
+INSERT INTO pics (pic_id,user_id) VALUES
+(DEFAULT, 'dcf7cee5-2f02-4cec-9090-618309c3714a' );
+
+--Check proper relation
+SELECT * FROM users LEFT JOIN pics ON users.user_id = pics.user_id; --use left join since all pics must belong to a user
