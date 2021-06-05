8d8e4e5f199564258f09d38afa7deef607ad6de6
ErnestCYW <ernestcyw99@gmail.com>
2021-06-05 19:11:53 +0800

    included sql comments and added pics

--- server/database.sql
+++ server/database.sql
@@ -1,6 +1,8 @@
-CREATE DATABASE ShutterSwipe;
+CREATE DATABASE shutterswipe;
 
 --BASED OF DATABASE SCHEMA
+
+--CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
 
 --User Table
 CREATE TABLE users (
@@ -26,7 +28,7 @@ INSERT INTO users (user_id, user_name, user_email, user_password) VALUES
 INSERT INTO pics (pic_id,user_id) VALUES
 (DEFAULT, 'dcf7cee5-2f02-4cec-9090-618309c3714a' ); --your uuid (user_id) will be different
 INSERT INTO pics (pic_id,user_id) VALUES
-(DEFAULT, 'dcf7cee5-2f02-4cec-9090-618309c3714a' );
+(DEFAULT, 'dcf7cee5-2f02-4cec-9090-618309c3714a' ); --SELECT user_id FROM users WHERE user_email = 'test@gmail.com' to see value
 
 --Check proper relation
 SELECT * FROM users LEFT JOIN pics ON users.user_id = pics.user_id; --use left join since all pics must belong to a user
