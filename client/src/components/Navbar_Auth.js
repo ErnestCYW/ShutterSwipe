037287d20401694c96ed5cea08b2522a76d29ecc
Timothy Wong <e0559903@u.nus.edu>
2021-06-19 14:09:08 +0800

    Add alternate Navbar

---
+++ client/src/components/Navbar_Auth.js
@@ -0,0 +1,55 @@
+import React from "react";
+import { toast } from "react-toastify";
+import Logo from "../Logo.svg";
+
+function NavbarAuth({ setAuth }) {
+  const logout = (e) => {
+    e.preventDefault();
+    localStorage.removeItem("token");
+    setAuth(false);
+    toast.success("Logged out successfully");
+  };
+
+  return (
+    <div>
+      <nav className="navbar navbar-expand-lg navbar-dark bg-dark sticky-top">
+        <div className="container-fluid">
+          <a className="navbar-brand" href="/">
+            <img src={Logo} alt="" width="62.5" height="50"></img>
+            ShutterSwipe
+          </a>
+          <button
+            className="navbar-toggler"
+            type="button"
+            data-bs-toggle="collapse"
+            data-bs-target="navbarNav"
+            aria-controls="navbarNav"
+            aria-expanded="false"
+            aria-label="Toggle navigation"
+          >
+            <span className="navbar-toggler-icon"></span>
+          </button>
+          <div className="collapse navbar-collapse" id="navbarNav">
+            <ul className="navbar-nav ml-auto">
+              <li className="nav-item">
+                <a className="nav-link" href="/dashboard">
+                  Home
+                </a>
+              </li>
+              <li className="nav-item">
+                <a className="nav-link" href="/feed">
+                  Feed
+                </a>
+              </li>
+            </ul>
+            <button className="btn btn-primary" onClick={(e) => logout(e)}>
+              Logout
+            </button>
+          </div>
+        </div>
+      </nav>
+    </div>
+  );
+}
+
+export default NavbarAuth;
---
+++ client/src/components/Navbar_Unauth.js
@@ -0,0 +1,44 @@
+import React from "react";
+import Logo from "../Logo.svg";
+
+function NavbarUnauth() {
+  return (
+    <div>
+      <nav className="navbar navbar-expand-lg navbar-dark bg-dark sticky-top">
+        <div className="container-fluid">
+          <a className="navbar-brand" href="/">
+            <img src={Logo} alt="" width="62.5" height="50"></img>
+            ShutterSwipe
+          </a>
+          <button
+            className="navbar-toggler"
+            type="button"
+            data-bs-toggle="collapse"
+            data-bs-target="navbarNav"
+            aria-controls="navbarNav"
+            aria-expanded="false"
+            aria-label="Toggle navigation"
+          >
+            <span className="navbar-toggler-icon"></span>
+          </button>
+          <div className="collapse navbar-collapse" id="navbarNav">
+            <ul className="navbar-nav ml-auto">
+              <li className="nav-item">
+                <a className="nav-link" href="/login">
+                  Login
+                </a>
+              </li>
+              <li className="nav-item">
+                <a className="nav-link" href="/register">
+                  Register
+                </a>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </nav>
+    </div>
+  );
+}
+
+export default NavbarUnauth;
