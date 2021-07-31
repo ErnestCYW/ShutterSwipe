b3fdee8fa93b03aca04b97c8284bf52a3aaf12e5
Timothy Wong <e0559903@u.nus.edu>
2021-07-31 11:42:27 +0800

    Change endpoint to window.location.hostname, Change cors:origin: PORT

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ const path = require("path");
 const server = http.createServer(app);
 const io = require("socket.io")(server, {
   cors: {
-    origin: "*",
+    origin: PORT,
     methods: ["GET", "POST"],
   },
 });
@@ -98,6 +98,6 @@ app.get("*", (req, res) => {
 });
 
 //change to app ? ? ?
-http.listen(PORT, () => {
+server.listen(PORT, () => {
   console.log(`server has started on port ${PORT}`);
 });
